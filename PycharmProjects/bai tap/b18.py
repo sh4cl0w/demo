be1fb015d4046df92ddf704bce4f98ed52f8a0f6
sh4cl0w <111275327+sh4cl0w@users.noreply.github.com>
2024-06-30 22:14:19 +0700

    Update b18.py

--- PycharmProjects/bai tap/b18.py
+++ PycharmProjects/bai tap/b18.py
@@ -1,6 +1,4 @@
-a=int(input("enter a:"))
-b=int(input("enter b:"))
-w,t=8,4
+
 
 def chuyen_so_thanh_mang(a,t,w):
     arr=list()
@@ -10,32 +8,29 @@ def chuyen_so_thanh_mang(a,t,w):
         a=a%2**((t-1)*w)
         t-=1
     return arr
-arrA=chuyen_so_thanh_mang(a,t,w)
-arrA.reverse()
-arrB=chuyen_so_thanh_mang(b,t,w)
-arrB.reverse()
-c=list()
-e=0
+def solve(a,b,w,t ):
 
-tmp=arrA[0]+arrB[0]
-if tmp > 2**w:
-    c.append(tmp%(2**w))
-    e=1
-else:
-    c.append(tmp)
-    e=0
-for i in range(1,t):
-    tmp = arrA[i] + arrB[i]+e
-    if tmp > 2 ** w:
-        c.append(tmp % (2 ** w))
-        e = 1
-    else:
-        c.append(tmp)
-        e = 0
-c.reverse()
-tup=tuple((e,c))
-print(tup)
-print(a+b)
+    arrA=chuyen_so_thanh_mang(a,t,w)
+    arrA.reverse()
+    arrB=chuyen_so_thanh_mang(b,t,w)
+    arrB.reverse()
+
+    c = []
+    epsilon = 0
+    e = pow(2, 8)
+    for i in range(t):
+        s = arrA[i] + arrB[i] + epsilon
+        x = s%e
+        if(s > e): epsilon = 1
+        else: epsilon = 0
+        c.append(x)
+    return (epsilon, c[::-1])
+if __name__=='__main__':
+    a=int(input("enter a:"))
+    b=int(input("enter b:"))
+    w,t=8,4
+    print(solve(a,b,w,t))
+
 
 #2902461432
 #3791651136
